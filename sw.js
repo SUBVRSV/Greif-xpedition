08b78baa0c874f010d7dc41ed633cd50248b363f
XNDL <50991908+SUBVRSV@users.noreply.github.com>
2026-04-08 20:32:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GREIF XPEDITION Krisenhandbuch - Service Worker
-const CACHE = 'greif-V13.88';
+const CACHE = 'greif-V14.0';
 const URLS = [self.location.pathname.replace('sw.js', '') || '/'];
 
 self.addEventListener('install', e => {
