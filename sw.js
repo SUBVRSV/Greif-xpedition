64d9f04fd10a974f56b679b9c41b527cbf849dd4
XNDL <tb@thomas-berger.biz>
2026-04-14 21:17:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GREIF XPEDITION Krisenhandbuch - Service Worker
-const CACHE = 'greif-V14.46';
+const CACHE = 'greif-V14.47';
 const URLS = [self.location.pathname.replace('sw.js', '') || '/'];
 
 self.addEventListener('install', e => {
