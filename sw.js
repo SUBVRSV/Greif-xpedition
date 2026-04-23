1cb344ed73ca35903d7c27f5a8f66d25eaa925f5
XNDL <tb@thomas-berger.biz>
2026-04-23 20:45:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GREIF XPEDITION Krisenhandbuch - Service Worker
-const CACHE = 'greif-V24.1';
+const CACHE = 'greif-V24.2';
 
 // Robuste URL-Erkennung: funktioniert auf GitHub Pages (Unterordner) und Root
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
