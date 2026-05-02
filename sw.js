c8317389ad13059710f7ba6ffbfc748c445d1cf7
XNDL <50991908+SUBVRSV@users.noreply.github.com>
2026-05-03 01:38:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GREIF XPEDITION Krisenhandbuch - Service Worker
-const CACHE = 'greif-V31.1';
+const CACHE = 'greif-V31.3';
 
 // Robuste URL-Erkennung: funktioniert auf GitHub Pages (Unterordner) und Root
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
