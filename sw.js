2d642c83495ae1ccdf9ad43cc7fe9f92244adc4c
XNDL <50991908+SUBVRSV@users.noreply.github.com>
2026-05-10 22:41:46 +0200

    sidebar scrollbalken fixed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GREIF XPEDITION Krisenhandbuch - Service Worker
-const CACHE = 'greif-V38.4';
+const CACHE = 'greif-V38.5';
 
 // Robuste URL-Erkennung: funktioniert auf GitHub Pages (Unterordner) und Root
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
